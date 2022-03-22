a2a790a3ccacae1b21665cdd6351822bf0d27675
Daniel Vizoli <vizoli.daniel96@gmail.com>
2022-03-22 14:16:00 -0300

    Ajustes botões em tela

--- js/main.js
+++ js/main.js
@@ -14,3 +14,11 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+function openPopup() {
+    document.getElementById("myPopup").style.display = "block";
+  }
+  
+  function closePopup() {
+    document.getElementById("myPopup").style.display = "none";
+  }
